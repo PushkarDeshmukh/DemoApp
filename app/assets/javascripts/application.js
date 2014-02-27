bdca42d4c4f27f98308a07c5cfd5e1dda6416617
Pushkar <pushkardeshmukh@hotmail.com>
2014-02-27 10:55:47 +0530

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,39 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//="jquery-1.8.3.min.js"
+//="jquery-slimscroll/jquery-ui-1.9.2.custom.min.js"
+//="jquery-slimscroll/jquery.slimscroll.min.js"
+//="fullcalendar/fullcalendar/fullcalendar.min.js"
+//="bootstrap/js/bootstrap.min.js"
+//="jquery.blockui.js"
+//="jquery.cookie.js"
+//="excanvas.js"
+//="respond.js"
+//="jqvmap/jqvmap/jquery.vmap.js" 
+//="jqvmap/jqvmap/maps/jquery.vmap.russia.js" 
+//="jqvmap/jqvmap/maps/jquery.vmap.world.js" 
+//="jqvmap/jqvmap/maps/jquery.vmap.europe.js" 
+//="jqvmap/jqvmap/maps/jquery.vmap.germany.js" 
+//="jqvmap/jqvmap/maps/jquery.vmap.usa.js" 
+//="jqvmap/jqvmap/data/jquery.vmap.sampledata.js" 
+//="jquery-knob/js/jquery.knob.js"
+//="flot/jquery.flot.js"
+//="flot/jquery.flot.resize.js"
+//="flot/jquery.flot.pie.js"
+//="flot/jquery.flot.stack.js"
+//="flot/jquery.flot.crosshair.js"
+//="jquery.peity.min.js"
+//="uniform/jquery.uniform.min.js"
+//="scripts.js"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdminApp::Application.config.secret_key_base = '3f1185b29b0597895d9a23bf6ec9929ad5a5454dba6d9509f96364b62220a7270602dfd0c26cf09781926ca28f176eb8425def44b3169ded7ace672a779fa559'
